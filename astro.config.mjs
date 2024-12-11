9c68ae17d6ba8cecd22f41101eed93609d6f69c6
reboiedo <javier@rebolledo.mx>
2024-12-11 13:51:23 +0100

    adjusted button sizes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  devToolbar: {
-    enabled: false,
-  },
+  // devToolbar: {
+  //   enabled: false,
+  // },
 });
